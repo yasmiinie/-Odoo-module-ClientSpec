01b52c5e955c69e022899d54328b4048bd7acf5c
yasmiinie <ly_zaidi@esi.dz>
2025-01-07 08:30:17 +0100

    changing client view form

--- models.py
+++ models.py
@@ -14,7 +14,24 @@ class Client(models.Model):
     
     commande_ids = fields.One2many(
         'clientspec.commande', 'client_id', string="Commandes")
-    is_local = fields.Boolean(string="Est Local ? " , default = True)
+    
+    #devoir maison added
+    is_local = fields.Boolean(string="Est Local ? " , default = True) 
+    client_type = fields.Selection([
+        ('particulier', 'Particulier'),
+        ('entreprise_publique', 'Entreprise Publique'),
+        ('entreprise_privee', 'Entreprise Privée'),
+    ],
+        string = "Type client"  , required = True , default = 'particulier')
+    
+    assurance_ids = fields.Many2many(
+        'clientspec.assurance', 
+        'client_assurance_rel',  # Nom de la table d'association
+        'client_id',  # Colonne pour les IDs des clients
+        'assurance_id',  # Colonne pour les IDs des assurances
+        string="Assurances"
+    )
+    
     
 
 class Commande(models.Model):
@@ -34,7 +51,14 @@ class Assurance(models.Model):
     label = fields.Char(string="IdAssurance", required=True)
     dateSouscription = fields.Date()
     piece = fields.Binary()    
-    client_id = fields.Many2one('clientspec.client',
-        ondelete='cascade', string="Client", required=True)
+    #client_id = fields.Many2one('clientspec.client', ondelete='cascade', string="Client", required=True)
+    
+    client_ids = fields.Many2many(
+        'clientspec.client', 
+        'client_assurance_rel',  # Nom de la table d'association (doit correspondre)
+        'assurance_id',  # Colonne pour les IDs des assurances
+        'client_id',  # Colonne pour les IDs des clients
+        string="Clients"
+    )
 
 
